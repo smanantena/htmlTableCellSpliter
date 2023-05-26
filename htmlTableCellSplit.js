08a433329e903ffd5c8a87c70054a0c8ee2aec44
Randriantsoa Manantena <randriantsoa.manantena@gmail.com>
2023-05-26 18:07:31 +0300

    Creating of the first user interface
    This is a simple ui.

--- htmlTableCellSplit.js
+++ htmlTableCellSplit.js
@@ -1,4 +1,5 @@
 //This file contain function to split cell of html table done
+/* This section is provide by codingame, the costum code will be come after that
 const NR = parseInt(readline());
 for (let i = 0; i < NR; i++) {
     const ROW = readline();
@@ -6,3 +7,4 @@ for (let i = 0; i < NR; i++) {
 var inputs = readline().split(' ');
 const IS = parseInt(inputs[0]);
 const DS = inputs[1];
+*/
